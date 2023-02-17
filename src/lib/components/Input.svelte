86c1a39fc6707dea2f40e03c003c6b6c2b3c5125
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-02-17 23:28:59 +0530

    login and signup ✅

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,82 @@
+<script>
+	import { IconAt, IconLock } from '@tabler/icons-svelte';
+
+	export let type = '';
+	export let placeholder = '';
+	export let value = '';
+	export let label = '';
+	export let minlength = 0;
+
+	function chooseIcon() {
+		switch (type) {
+			case 'email':
+				return IconAt;
+			case 'password':
+				return IconLock;
+			default:
+				return null;
+		}
+	}
+
+	function typeAction(e) {
+		e.type = type;
+	}
+
+	const Icon = chooseIcon();
+</script>
+
+<div class="row">
+	<label for="email">{label}</label>
+	<div class="input">
+		{#if Icon}
+			<div class="icon"><Icon size={20} /></div>
+		{/if}
+		<input use:typeAction bind:value {placeholder} required />
+	</div>
+</div>
+
+<style lang="scss">
+	label {
+		display: block;
+		margin-bottom: 0.25rem;
+	}
+	.row {
+		display: flex;
+		flex-direction: column;
+		gap: 0.35rem;
+	}
+	.icon,
+	svg {
+		display: inline-block;
+	}
+	.icon {
+		background-color: hsl(34 61% 90%);
+		block-size: 2.75rem;
+		inline-size: 3rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 0.3rem 0 0 0.3rem;
+	}
+	.input {
+		display: flex;
+		align-items: center;
+		block-size: 2.75rem;
+		border-radius: 0.3rem;
+
+		&:focus-within {
+			outline: 2px solid var(--purple);
+		}
+	}
+	input {
+		inline-size: 100%;
+		block-size: 100%;
+		border: none;
+		background-color: hsl(33 60% 92%);
+		outline: none;
+		padding: 0 0.5rem;
+		font-size: inherit;
+		color: inherit;
+		border-radius: 0 0.3rem 0.3rem 0;
+	}
+</style>
--- src/routes/account/signup/+page.svelte
+++ src/routes/account/signup/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { goto } from '$app/navigation';
+	import Input from '$lib/components/Input.svelte';
 	import { auth } from '$lib/utils/authStore';
 
 	let email = '';
@@ -15,13 +16,53 @@
 	<title>Sign Up | Bibliophile</title>
 </svelte:head>
 
-<h1>create new account</h1>
-<form on:submit|preventDefault={submit}>
-	<label for="email">Email</label>
-	<input type="email" bind:value={email} required />
-	<label for="password">Password</label>
-	<input type="password" bind:value={password} required />
-	<button>Login</button>
-</form>
+<section class="signup">
+	<h1>Create a new account</h1>
+	<form on:submit|preventDefault={submit}>
+		<Input type="email" label="Email" bind:value={email} />
+		<Input type="password" label="Password" bind:value={password} />
+		<button>Create account</button>
+	</form>
+	<p>Already have an account? <a href="/account/login">Login</a></p>
+</section>
 
-<p>Already have an account? <a href="/account/login">Login</a></p>
+
+<style lang="scss">
+	section {
+		h1 {
+			text-align: center;
+		}
+
+		@media (min-width: 750px) {
+			margin-top: calc(-1 * var(--nav));
+			h1 {
+				text-align: left;
+			}
+		}
+	}
+
+	form {
+		inline-size: min(100%, clamp(20rem, 100%, 25rem));
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		margin-top: 1.5rem;
+	}
+
+	button {
+		background-color: var(--purple);
+		color: var(--background);
+		border: none;
+		border-radius: 0.25rem;
+		padding: 0.7rem 1rem;
+		font-size: inherit;
+		cursor: pointer;
+		text-transform: uppercase;
+		letter-spacing: 2px;
+		font-size: var(--fs--200)
+	}
+
+	p {
+		margin-top: 1rem;
+	}
+</style>
