07b72e914d415dfd3d7a5c26b7d08a908a728209
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-02-20 18:13:32 +0530

    changed bookcover folder

---
+++ src/routes/bookshelf/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import bookData from '$lib/books.json';
+	console.log(bookData)
+	let bookUrl = bookData.at(-1)?.url;
+	console.log(bookUrl)
+</script>
+
+<main>
+	<h1>Bookshelf Page</h1>
+	<img src={bookUrl} alt="a book cover">
+</main>
