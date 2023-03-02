5ed66d11158171a32a2582efa34579a36c54215f
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-03-02 23:19:29 +0530

    filter menu changes

--- src/lib/utils/PageTransition.svelte
+++ src/lib/utils/PageTransition.svelte
@@ -4,7 +4,7 @@
 </script>
 
 {#key pathname}
-	<div in:fly={{ x: -10, duration: 250, delay: 200 }} out:fly={{ x: 5, duration: 200 }}>
+	<div class="page" in:fly={{ x: -10, duration: 250, delay: 200 }} out:fly={{ x: 5, duration: 200 }}>
 		<slot />
 	</div>
 {/key}
